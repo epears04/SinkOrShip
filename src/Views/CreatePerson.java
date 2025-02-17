dac95d306f57661eafd04b418728e6d9e364aa93
MStavr <mtstavros12@gmail.com>
2025-02-17 14:56:16 -0800

    adding the views folder and basic additions ot the createPerson page

--- src/Views/CreatePerson.java
+++ src/Views/CreatePerson.java
@@ -9,28 +9,31 @@ public class CreatePerson implements ActionListener {
     private JTextField nameField;
     private JPanel panel;
     private boolean visibility = false;
+    private JTextField ageField;
+    private JTextField genderField;
+    private JTextField imageUpload;
+    private JTextField personalDescriptionField;
 
     public CreatePerson() {
         frame = new JFrame("Create Person");
         panel = new JPanel();
-        panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); //ensures that the frame actually closes all the way when clicking X
         frame.setSize(1000, 1000);
         frame.setLocationRelativeTo(null); //centers on screen
         nameField = new JTextField(20);
+        ageField = new JTextField(20);
+        genderField = new JTextField(20);
+        imageUpload = new JTextField(20);
+        personalDescriptionField = new JTextField(20);
         JLabel nameLabel = new JLabel("Username:");
         JButton submitButton = new JButton("Submit");
         submitButton.addActionListener(this);
 
-//        JTextField ageField = new JTextField();
-//        JTextField genderField = new JTextField();
-//        JTextField imageUpload = new JTextField();
-//        JTextField personalDescriptionField = new JTextField();
+
         panel.add(nameLabel);
         panel.add(nameField);
         panel.add(submitButton);
         frame.add(panel);
-
     }
 
     public void toggleShow(){
