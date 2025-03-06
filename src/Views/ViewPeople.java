09171a1f5bcf34334febfa8658f7f41382ef5728
Ellie Pearson <ellie.jm.pearson@gmail.com>
2025-03-06 14:33:11 -0800

    added image

--- src/Views/ViewPeople.java
+++ src/Views/ViewPeople.java
@@ -5,6 +5,7 @@ import MainPack.MainFrame;
 import javax.swing.*;
 import java.awt.*;
 import java.sql.*;
+import java.io.*;
 
 public class ViewPeople extends JPanel {
 
@@ -24,10 +25,16 @@ public class ViewPeople extends JPanel {
         JScrollPane scrollPane = new JScrollPane(panel);
         add(scrollPane, BorderLayout.CENTER);
 
+        //add title
+        JLabel title = new JLabel("People");
+        title.setFont(new Font("Arial", Font.BOLD, 24));
+        panel.add(title, BorderLayout.CENTER); //need to make this align center
+        panel.add(Box.createVerticalStrut(30));
+
         fetchPeople();
     }
 
-    // username, age, major, gender, profile_pic (blob), description
+    // get person's username, age, major, gender, profile_pic (blob), description from database
     public void fetchPeople() {
         try {
             Connection connect = Connect.createConnection();
@@ -35,13 +42,14 @@ public class ViewPeople extends JPanel {
             PreparedStatement statement = connect.prepareStatement(query);
             ResultSet resultSet = statement.executeQuery();
             while (resultSet.next()) {
-                String username = resultSet.getString("username");
-                int age = resultSet.getInt("age");
-                String gender = resultSet.getString("gender");
-                Blob imgBlob = resultSet.getBlob("profile_pic");
-                String description = resultSet.getString("description");
+                String username = resultSet.getString(1);
+                int age = resultSet.getInt(2);
+                String major = resultSet.getString(3);
+                String gender = resultSet.getString(4);
+                Blob imgBlob = resultSet.getBlob(5);
+                String description = resultSet.getString(6);
 
-                addPerson(username, age, gender, imgBlob, description);
+                addPerson(username, age, gender, major, imgBlob, description);
             }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -49,27 +57,74 @@ public class ViewPeople extends JPanel {
         }
     }
 
-    public void addPerson(String username, int age, String gender, Blob blob, String description) {
+    // get image from Blob or default image
+    private ImageIcon getProfileImage(Blob blob) {
+        try {
+            ImageIcon profileIcon;
+
+            if (blob != null) {
+                byte[] imgData = blob.getBytes(1, (int) blob.length());
+                profileIcon = new ImageIcon(imgData);
+            } else {
+                profileIcon = new ImageIcon("images/default_profile.png");
+            }
+
+            // Resize the image to 64x64
+            Image scaledImage = profileIcon.getImage().getScaledInstance(64, 64, Image.SCALE_SMOOTH);
+            return new ImageIcon(scaledImage);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        ImageIcon fallbackIcon = new ImageIcon("images/default_profile.png");
+        Image scaledFallback = fallbackIcon.getImage().getScaledInstance(64, 64, Image.SCALE_SMOOTH);
+        return new ImageIcon(scaledFallback);
+    }
+
+    // add Person data to page
+    public void addPerson(String username, int age, String gender, String major, Blob blob, String description) {
+        // create person box
         JPanel personPanel = new JPanel();
-        personPanel.setLayout(new BoxLayout(personPanel, BoxLayout.Y_AXIS));
-        personPanel.setPreferredSize(new Dimension(800, 100));
+        personPanel.setLayout(new BoxLayout(personPanel, BoxLayout.X_AXIS));
+        personPanel.setPreferredSize(new Dimension(800, 150));
         personPanel.setBorder(BorderFactory.createEmptyBorder(5, 20, 5, 30));
 
+        // Profile Image Handling
+        JPanel imagePanel = new JPanel();
+        imagePanel.setLayout(new BoxLayout(imagePanel, BoxLayout.Y_AXIS));
+        JLabel imageLabel = new JLabel();
+        ImageIcon profileImage = getProfileImage(blob);
+        imageLabel.setIcon(profileImage);
+        imagePanel.add(imageLabel);
+
+        // create info labels
         JLabel nameLabel = new JLabel(username);
         nameLabel.setFont(nameLabel.getFont().deriveFont(Font.BOLD, 20));
+        JLabel majorLabel = new JLabel("Major: " + major, JLabel.CENTER);
         JLabel ageLabel = new JLabel("Age: " + age, JLabel.CENTER);
         JLabel genderLabel = new JLabel("Gender: " + gender, JLabel.CENTER);
         JLabel descriptionLabel = new JLabel("Description: " + description, JLabel.CENTER );
 
-        personPanel.add(nameLabel);
-        personPanel.add(Box.createVerticalStrut(5));
-        personPanel.add(ageLabel);
-        personPanel.add(Box.createVerticalStrut(5));
-        personPanel.add(genderLabel);
-        personPanel.add(Box.createVerticalStrut(5));
-        personPanel.add(descriptionLabel);
-        personPanel.add(Box.createVerticalStrut(5));
+        // add info labels to box
+        JPanel infoPanel = new JPanel();
+        infoPanel.setLayout(new BoxLayout(infoPanel, BoxLayout.Y_AXIS));
+        infoPanel.add(nameLabel);
+        infoPanel.add(Box.createVerticalStrut(5));
+        infoPanel.add(ageLabel);
+        infoPanel.add(Box.createVerticalStrut(5));
+        infoPanel.add(genderLabel);
+        infoPanel.add(Box.createVerticalStrut(5));
+        infoPanel.add(majorLabel);
+        infoPanel.add(Box.createVerticalStrut(5));
+        infoPanel.add(descriptionLabel);
+        infoPanel.add(Box.createVerticalStrut(5));
 
+        personPanel.add(imagePanel, BorderLayout.WEST);
+        personPanel.add(Box.createHorizontalStrut(30));
+        personPanel.add(infoPanel, BorderLayout.CENTER);
+
+        // update people panel
         panel.add(personPanel);
         panel.add(Box.createVerticalStrut(30));
         panel.revalidate();
